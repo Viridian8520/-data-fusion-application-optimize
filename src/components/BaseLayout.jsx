c09fd2b3eb59f82daa632174d010825632c6c9f1
yangxinnan <2331701170@qq.com>
2023-10-12 19:11:58 +0800

    feat/queryData:修改请求数据顺序

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react';
-import { Layout, Menu, Button } from 'antd';
-import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
 import { Routes, Route, Navigate, } from 'react-router-dom'
 import { menuConfig } from '../routes/routerConfig';
 import Detail from '../pages/Details'
 
 const { Header, Sider, Content } = Layout;
 function BaseLayout() {
-    const [collapsed, setCollapsed] = useState(false);
 
     return (
         <Layout
-            style={{
-                height: '100vh'
-            }}
+            hasSider
         >
             <Sider
                 trigger={null}
                 collapsible
-                collapsed={collapsed}
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
             >
                 <Menu
                     theme='dark'
@@ -30,7 +29,12 @@ function BaseLayout() {
                     items={menuConfig}
                 />
             </Sider>
-            <Layout>
+            <Layout
+                className="site-layout"
+                style={{
+                    marginLeft: 200,
+                }}
+            >
                 <Header
                     style={{
                         padding: 0,
@@ -40,16 +44,6 @@ function BaseLayout() {
                         backgroundColor: "white"
                     }}
                 >
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                        }}
-                    />
                 </Header>
                 <Content
                     className="site-layout-background"
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,20 +1,30 @@
-import { useState, Suspense } from 'react';
+import { useState, Suspense, useEffect } from 'react';
 import Optimize from "./Optimize";
 import { queryOptimizeData } from './Optimize/ActionCreator';
 import Loading from '../../components/Loading'
 
 // eslint-disable-next-line react/prop-types
 export default function Detail({ id }) {
-    // eslint-disable-next-line no-unused-vars
-    const [corporation, setCorporation] = useState('小丫家电')
-    queryOptimizeData().then(res => {
-        localStorage.setItem('optimizeData', JSON.stringify(res.data));
-    })
+    const [isLoading, setIsLoading] = useState(true);
+    // const [optimizeData, setOptimizeData] = useState({});
+    const [corporation, setCorporation] = useState('')
+
+    useEffect(() => {
+        queryOptimizeData().then(res => {
+            localStorage.setItem('optimizeData', JSON.stringify(res.data));
+            // setOptimizeData(res.data);
+            setCorporation(Object.entries(res.data.corporationMap)[0][0]);
+            setIsLoading(false);
+        })
+    }, [])
+
     return (
         <Suspense fallback={<Loading />}>
-            <div id={id}>
-                <Optimize corporation={corporation} />
-            </div>
+            {isLoading ? <Loading /> :
+                <div id={id}>
+                    <Optimize corporation={corporation} />
+                </div>
+            }
         </Suspense>
     )
 }
